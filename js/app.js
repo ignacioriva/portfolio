26c8bf74ff4701c4973c782db5b2dc1c85f2b9d5
ignacioriva <84292438+ignacioriva@users.noreply.github.com>
2022-06-13 18:17:46 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,75 @@
+function darkMode () {
+
+	let darkMode = localStorage.getItem("darkMode");
+	const darkModeToggle = document.querySelector("#dark-mode");
+
+	function enableDarkMode () {
+		let body = document.getElementsByTagName("body")[0];
+		body.classList.add("darkMode");
+		localStorage.setItem("darkMode", "enabled");
+		let moon = document.querySelector('.fa-moon').style.visibility = 'hidden';
+		let sun = document.querySelector('.fa-sun').style.visibility = 'visible';
+	}
+
+	function disableDarkMode () {
+		let body = document.getElementsByTagName("body")[0];
+		body.classList.remove("darkMode");
+		localStorage.setItem("darkMode", null);
+		let sun = document.querySelector('.fa-sun').style.visibility = 'hidden';
+		let moon = document.querySelector('.fa-moon').style.visibility = 'visible';
+
+
+	}
+
+	darkModeToggle.addEventListener("click", function () {
+		darkMode = localStorage.getItem("darkMode");
+		if (darkMode !== "enabled") {
+			enableDarkMode();
+		} else {
+			disableDarkMode();
+		}
+		});
+}
+
+darkMode();
+
+
+
+
+
+	const sr = ScrollReveal({
+    distance: '90px',
+    duration: 3000,
+})
+
+sr.reveal(`.content-div`, {origin: 'top', delay: 300})
+sr.reveal(`.image-div`, {origin: 'bottom', delay: 500})
+sr.reveal(`.title-div`, {origin: 'right', delay: 300})
+
+
+
+
+// const intervalSection2 = () => { setInterval( () => {
+// 	let section2 = window.getComputedStyle(document.querySelector('.section-2')).display;
+// 	 if (section2 == 'flex') {
+//  	sr.reveal(`.content-div-2`, {origin: 'top', delay: 300})
+//  	clearInterval(intervalSection2); //paramos la ejecución del check
+
+//  }
+// }, 4000)};
+
+// const intervalSection3 = () => { setInterval( () => {
+// 	let section3 = window.getComputedStyle(document.querySelector('.section-3')).display;
+// 	 if (section3 == 'flex') {
+//  	sr.reveal(`.content-div-3`, {origin: 'top', delay: 300})
+//  	clearInterval(intervalSection1); //paramos la ejecución del check
+
+//  }
+// }, 1000)};
+
+// intervalSection2();
+// intervalSection3();
+
+
+
+
